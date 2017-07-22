c831de9a230f5ba62228f941747049fa9ff37e35
rohitas_b <rohitas_b@MGTS-IT-04>
2017-07-22 16:52:16 +0530

    Add project files.

---
+++ DotNet_signalR/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DotNet_signalR.MvcApplication" Language="C#" %>
---
+++ DotNet_signalR/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DotNet_signalR.Startup))]
+namespace DotNet_signalR
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
