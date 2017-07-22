aa2f506dac6596f4f1a25cbd8422afc63bb25c2d
rohitas_b <rohitas_b@MGTS-IT-04>
2017-07-22 17:02:52 +0530

    first

--- DotNet_signalR/App_Start/RouteConfig.cs
+++ DotNet_signalR/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace DotNet_signalR
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Chat", id = UrlParameter.Optional }
             );
         }
     }
--- DotNet_signalR/Controllers/HomeController.cs
+++ DotNet_signalR/Controllers/HomeController.cs
@@ -26,5 +26,10 @@ namespace DotNet_signalR.Controllers
 
             return View();
         }
+
+        public ActionResult Chat()
+        {
+            return View();
+        }
     }
 }
